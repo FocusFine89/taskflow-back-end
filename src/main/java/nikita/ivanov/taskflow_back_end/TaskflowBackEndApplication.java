1465cb4e18c47d89abecfe8a127b4bacfe7fc2fb
FocusFine89 <niivanov02@gmail.com>
2024-07-15 14:10:26 +0200

    setup application

---
+++ src/main/java/nikita/ivanov/taskflow_back_end/TaskflowBackEndApplication.java
@@ -0,0 +1,13 @@
+package nikita.ivanov.taskflow_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowBackEndApplication.class, args);
+	}
+
+}
