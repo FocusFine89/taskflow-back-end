35563d956172cd8282451cc1ef85b9aa07607cef
FocusFine89 <niivanov02@gmail.com>
2024-07-20 15:50:31 +0200

    aggiunti metodi Controller per gli User

--- src/main/java/nikita/ivanov/taskflow_back_end/users/UsersController.java
+++ src/main/java/nikita/ivanov/taskflow_back_end/users/UsersController.java
@@ -1,8 +1,14 @@
 package nikita.ivanov.taskflow_back_end.users;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -10,6 +16,36 @@ public class UsersController {
     @Autowired
     UsersService usersService;
 
+    // end point /me per vedere il proprio profilo
+    @GetMapping("/me")
+    @ResponseStatus(HttpStatus.OK)
+    public Users getUserProfile(@AuthenticationPrincipal Users currentUser){
+        return this.usersService.findById(currentUser.getId());
+    }
+
+    //Lista di tutti gli utenti (solo admin hanno accesso a questo end-point)
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Page<Users> getAllUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "30") int size){
+        return this.usersService.findAllUsers(page, size);
+    }
+
+    //Modifica Utente
+    @PostMapping("/update")
+    @ResponseStatus(HttpStatus.OK)
+    public Users updateUser(@RequestBody UsersUpdateDTO updatedUser, @AuthenticationPrincipal Users user){
+        return this.usersService.findByIdAndUpdate(updatedUser, user);
+    }
+
+    //Elimina un utente (Solo gli admin possono eliminare un utente)
+    //TODO eliminare tutte le cose collegate all'utente a cascata
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void FindUserByIdAndDelete(@PathVariable long id){
+        this.usersService.findByIdAndDelete(id);
+    }
 
 
 }
